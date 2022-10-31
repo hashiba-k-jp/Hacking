8ba11bcbc505fc95cb59e411cc7cc59c79c21b9c
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2022-11-01 01:31:12 +0900

    p36

---
+++ 0x200/use_lldb.sh
@@ -0,0 +1,33 @@
+# How to debug
+# USE lldb INSTEAD OF gdb (MacOS)
+# https://lldb.llvm.org/use/map.html
+
+gcc firstprog.c
+lldb ./a.out
+
+# lldb will start
+# and "(lldb)" will appear instead of "%"(zsh)
+
+# ---------- These commands can not be done automatically!! ----------
+# (gdb) break main
+breakpoint set --name main
+# (gdb) run
+run
+# (gdb) info registers
+register read
+
+# (dgb) disassemble main
+disassemble --name main
+# (gdb) i r eip  -> (gdb) info register eip
+# ip stands for "instruction pointer".
+register read rip
+# (gdb) examine/x $eip
+# check the value of $eip, and get output in hexadecimal(16).
+x/x $rip
+# (gdb) x/2x $eip
+# get groups of data in a row; this example is 2.
+x/2x $rip
+
+
+
+# And you need "exit" for kill task the lldb
