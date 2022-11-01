4b59b68897008d5e3586692a70670ed45ce25037
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2022-11-02 00:10:22 +0900

    save

--- 0x200/use_lldb.sh
+++ 0x200/use_lldb.sh
@@ -8,7 +8,9 @@ lldb ./a.out
 # lldb will start
 # and "(lldb)" will appear instead of "%"(zsh)
 
-# ---------- These commands can not be done automatically!! ----------
+# ---------- These commands can not be done automatically
+# when this file is run as shellscript!! ----------
+
 # (gdb) break main
 breakpoint set --name main
 # (gdb) run
@@ -18,7 +20,7 @@ register read
 
 # (dgb) disassemble main
 disassemble --name main
-# (gdb) i r eip  -> (gdb) info register eip
+# (gdb) i r eip  := (gdb) info register eip
 # ip stands for "instruction pointer".
 register read rip
 # (gdb) examine/x $eip
@@ -27,7 +29,13 @@ x/x $rip
 # (gdb) x/2x $eip
 # get groups of data in a row; this example is 2.
 x/2x $rip
+# (gdb) x/i $eip
+# show hex value and its assembla
+x/i $rip
 
+# (dgb) nexti
+# NEXTInstruction
+nexti
 
 
 # And you need "exit" for kill task the lldb
